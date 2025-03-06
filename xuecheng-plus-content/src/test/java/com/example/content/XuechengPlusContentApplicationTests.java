fc4e94998d92778546c857084bb1fe61c990fd02
seven2343 <346218514@qq.com>
2025-03-06 23:25:13 +0800

    添加content文件

---
+++ xuecheng-plus-content/src/test/java/com/example/content/XuechengPlusContentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.content;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XuechengPlusContentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
